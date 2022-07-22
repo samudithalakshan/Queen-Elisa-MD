a93837c70012d2ed42efcfad5f1e5c8c35058c5a
samudithalakshan <109779205+samudithalakshan@users.noreply.github.com>
2022-07-22 16:32:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,18 +32,18 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.owner = ['94761676948'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94761676948'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94761676948'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = '𝗠𝗔𝗦𝗧𝗘𝗥 𝗕𝗟𝗔𝗖𝗞' // Your name ඔබේ නම 
+global.botnma = '⛓️𝗠𝗔𝗦𝗧𝗘𝗥 𝗕𝗟𝗔𝗖𝗞⚔️' // Your bot name , ඔබගෙ බොට්ගේ නම 
 global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
 global.packname = 'Queen Elisa Sticker' // Sticker package name 
 global.author = 'Dark Maker' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
-// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
+// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` 🔒𝗜 𝗔𝗠 🔰𝗠𝗔𝗦𝗧𝗘𝗥 𝗕𝗟𝗔𝗖𝗞🔰` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
 global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
 global.alive =`Hello i am alive now
 
@@ -60,7 +60,7 @@ global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `𝙱𝚈 ⛓️𝗠𝗔𝗦𝗧𝗘𝗥 𝗕𝗟𝗔𝗖𝗞⚔️`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
